d078212e0c95ec77aa9c2470be1a58cb783ffaae
gabe <ggetzie@gmail.com>
2020-11-17 09:45:19 +0800

    add types to admin, list displays

--- main/admin.py
+++ main/admin.py
@@ -1,28 +1,64 @@
 from django.contrib import admin
 
 from main.models import (SpatialArea, SpatialContext, ObjectFind,
-                         MaterialCategory, ContextPhoto)
+                         MaterialCategory, ContextPhoto, AreaType, ContextType)
 
 @admin.register(SpatialArea)
 class SpatialAreaAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["utm_hemisphere",
+                    "utm_zone",
+                    "area_utm_easting_meters",
+                    "area_utm_northing_meters"]
+    list_display_links = ["utm_hemisphere",
+                          "utm_zone",
+                          "area_utm_easting_meters",
+                          "area_utm_northing_meters"]
 
 
 @admin.register(SpatialContext)
 class SpatialContextAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["utm_hemisphere",
+                    "utm_zone",
+                    "area_utm_easting_meters",
+                    "area_utm_northing_meters",
+                    "context_number"]
+    list_display_links = ["utm_hemisphere",
+                          "utm_zone",
+                          "area_utm_easting_meters",
+                          "area_utm_northing_meters",
+                          "context_number"]    
 
-
+    
 @admin.register(ObjectFind)
 class ObjectFindAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["utm_hemisphere",
+                    "utm_zone",
+                    "area_utm_easting_meters",
+                    "area_utm_northing_meters",
+                    "context_number",
+                    "find_number"]
+    list_display_links = ["utm_hemisphere",
+                          "utm_zone",
+                          "area_utm_easting_meters",
+                          "area_utm_northing_meters",
+                          "context_number",
+                          "find_number"]
 
 
 @admin.register(MaterialCategory)
 class MaterialCategoryAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["material", "category"]
+    list_display_links = ["material", "category"]
 
 
 @admin.register(ContextPhoto)
 class ContextPhotoAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(AreaType)
+class AreaTypeAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(ContextType)
+class ContextTypeAdmin(admin.ModelAdmin):
     pass
