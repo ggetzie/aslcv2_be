bd0fe5fccc058acb26954b5f0fb0ec80addd585e
Gabe Getzie <ggetzie@gmail.com>
2021-07-12 19:43:52 +0800

    file upload permissions

--- main/utils.py
+++ main/utils.py
@@ -3,9 +3,13 @@ from django.conf import settings
 import requests
 
 def get_token(username, password, url="http://aslcv2/auth-token/"):
-    r = requests.post(url,
-                      data={"username":username,
-                            "password": password})
+    r = requests.post(
+        url,  
+        data={
+            "username":username,
+            "password": password
+            })
+            
     if r.status_code == 200:
         headers = {"Authorization": f"Token {r.json()['token']}"}
         return headers
