817285c423336361d488b18611ab30c6c8ddcca5
Gabriel Getzie <ggetzie@hku.hk>
2023-09-13 16:54:58 +0800

    initial admin models for SurveyPath and SurveyPoint

--- main/admin.py
+++ main/admin.py
@@ -1,57 +1,85 @@
 from django.contrib import admin
 
-from main.models import (SpatialArea, SpatialContext, ObjectFind,
-                         MaterialCategory, ContextPhoto, AreaType, ContextType,
-                         ActionLog, BagPhoto, FindPhoto)
+from main.models import (
+    SpatialArea,
+    SpatialContext,
+    ObjectFind,
+    MaterialCategory,
+    ContextPhoto,
+    AreaType,
+    ContextType,
+    ActionLog,
+    BagPhoto,
+    FindPhoto,
+    SurveyPath,
+    SurveyPoint,
+)
+
 
 @admin.register(SpatialArea)
 class SpatialAreaAdmin(admin.ModelAdmin):
-    list_display = ["utm_hemisphere",
-                    "utm_zone",
-                    "area_utm_easting_meters",
-                    "area_utm_northing_meters"]
-    list_display_links = ["utm_hemisphere",
-                          "utm_zone",
-                          "area_utm_easting_meters",
-                          "area_utm_northing_meters"]
-    list_filter = ["utm_hemisphere",
-                   "utm_zone",
-                   "area_utm_easting_meters",
-                   "area_utm_northing_meters"]
+    list_display = [
+        "utm_hemisphere",
+        "utm_zone",
+        "area_utm_easting_meters",
+        "area_utm_northing_meters",
+    ]
+    list_display_links = [
+        "utm_hemisphere",
+        "utm_zone",
+        "area_utm_easting_meters",
+        "area_utm_northing_meters",
+    ]
+    list_filter = [
+        "utm_hemisphere",
+        "utm_zone",
+        "area_utm_easting_meters",
+        "area_utm_northing_meters",
+    ]
 
 
 @admin.register(SpatialContext)
 class SpatialContextAdmin(admin.ModelAdmin):
-    list_display = ["utm_hemisphere",
-                    "utm_zone",
-                    "area_utm_easting_meters",
-                    "area_utm_northing_meters",
-                    "context_number"]
-    list_display_links = ["utm_hemisphere",
-                          "utm_zone",
-                          "area_utm_easting_meters",
-                          "area_utm_northing_meters",
-                          "context_number"]
-    list_filter = ["utm_hemisphere",
-                    "utm_zone",
-                    "area_utm_easting_meters",
-                    "area_utm_northing_meters"]
+    list_display = [
+        "utm_hemisphere",
+        "utm_zone",
+        "area_utm_easting_meters",
+        "area_utm_northing_meters",
+        "context_number",
+    ]
+    list_display_links = [
+        "utm_hemisphere",
+        "utm_zone",
+        "area_utm_easting_meters",
+        "area_utm_northing_meters",
+        "context_number",
+    ]
+    list_filter = [
+        "utm_hemisphere",
+        "utm_zone",
+        "area_utm_easting_meters",
+        "area_utm_northing_meters",
+    ]
 
-    
+
 @admin.register(ObjectFind)
 class ObjectFindAdmin(admin.ModelAdmin):
-    list_display = ["utm_hemisphere",
-                    "utm_zone",
-                    "area_utm_easting_meters",
-                    "area_utm_northing_meters",
-                    "context_number",
-                    "find_number"]
-    list_display_links = ["utm_hemisphere",
-                          "utm_zone",
-                          "area_utm_easting_meters",
-                          "area_utm_northing_meters",
-                          "context_number",
-                          "find_number"]
+    list_display = [
+        "utm_hemisphere",
+        "utm_zone",
+        "area_utm_easting_meters",
+        "area_utm_northing_meters",
+        "context_number",
+        "find_number",
+    ]
+    list_display_links = [
+        "utm_hemisphere",
+        "utm_zone",
+        "area_utm_easting_meters",
+        "area_utm_northing_meters",
+        "context_number",
+        "find_number",
+    ]
 
 
 @admin.register(MaterialCategory)
@@ -65,8 +93,14 @@ class ContextPhotoAdmin(admin.ModelAdmin):
     list_display = ["__str__", "user", "created"]
     list_display_links = ["__str__"]
     readonly_fields = ["created", "thumbnail"]
-    ordering = ["utm_hemisphere", "utm_zone", "area_utm_easting_meters",
-                "area_utm_northing_meters", "context_number", "created"]
+    ordering = [
+        "utm_hemisphere",
+        "utm_zone",
+        "area_utm_easting_meters",
+        "area_utm_northing_meters",
+        "context_number",
+        "created",
+    ]
 
 
 @admin.register(BagPhoto)
@@ -75,30 +109,54 @@ class BagPhotoAdmin(admin.ModelAdmin):
     list_display_links = ["__str__"]
     readonly_fields = ["created", "thumbnail"]
     list_filter = ["source"]
-    ordering = ["utm_hemisphere", "utm_zone", "area_utm_easting_meters",
-                "area_utm_northing_meters", "context_number", "source", 
-                "created"]
+    ordering = [
+        "utm_hemisphere",
+        "utm_zone",
+        "area_utm_easting_meters",
+        "area_utm_northing_meters",
+        "context_number",
+        "source",
+        "created",
+    ]
+
 
 @admin.register(FindPhoto)
 class FindPhotoAdmin(admin.ModelAdmin):
     list_display = ["__str__", "user", "created"]
     list_display_links = ["__str__"]
     readonly_fields = ["created"]
-    ordering = ["utm_hemisphere", "utm_zone", "area_utm_easting_meters",
-                "area_utm_northing_meters", "context_number", "find_number", 
-                "created"]
-    
+    ordering = [
+        "utm_hemisphere",
+        "utm_zone",
+        "area_utm_easting_meters",
+        "area_utm_northing_meters",
+        "context_number",
+        "find_number",
+        "created",
+    ]
+
 
 @admin.register(AreaType)
 class AreaTypeAdmin(admin.ModelAdmin):
     pass
 
+
 @admin.register(ContextType)
 class ContextTypeAdmin(admin.ModelAdmin):
     pass
+
 
 @admin.register(ActionLog)
 class ActionLogAdmin(admin.ModelAdmin):
     list_display = ["timestamp", "model_name", "user"]
     list_display_links = ["timestamp", "model_name", "user"]
     readonly_fields = ["timestamp", "user", "model_name", "action", "object_id"]
+
+
+class SurveyPointInline(admin.TabularInline):
+    model = SurveyPoint
+
+
+@admin.register(SurveyPath)
+class SurveyPathAdmin(admin.ModelAdmin):
+    list_display = ["id", "user"]
